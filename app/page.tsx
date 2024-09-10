967b03f7f04d6dfe76cada5a27d756f53706dd52
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2024-09-10 09:15:46 +0100

    created flashcard route with link from home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-
+import Link from "next/link"
 
 export default function Page() {
     return (
         <main>
-            <p>Hello</p>
+            <Link href="/flashcards">Click here for flashcards</Link>
         </main>
 
     )
